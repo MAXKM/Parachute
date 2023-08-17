8b6a256792140e13e4b79f8ed8819ff5e83d8c62
2122031-PC\sasaki <s2122031@cco.kanagawa-it.ac.jp>
2023-08-17 15:49:02 +0900

    リングをくぐったときの判定

--- Assets/Sasaki/sScripts/ScoreMane.cs
+++ Assets/Sasaki/sScripts/ScoreMane.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreMane : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class ScoreMane : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //scoreNum = 0;
+        scoreNum = 0;
     }
 
     // Update is called once per frame
@@ -24,7 +25,24 @@ public class ScoreMane : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        scoreNum = scoreNum + 10;
-        Debug.Log("yes");
+        if(other.CompareTag("Add"))
+        {
+            scoreNum = scoreNum + 10;
+            Debug.Log("+");
+        }
+
+        if (other.CompareTag("Decrease"))
+        {
+            scoreNum = scoreNum - 10;
+            Debug.Log("-");
+        }
+
+        if (other.CompareTag("GOver"))
+        {
+            Debug.Log("no");
+            PlayerPrefs.SetInt("fscore", scoreNum);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("sResultScene");
+        }
     }
 }
